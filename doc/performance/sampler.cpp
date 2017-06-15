8da14bbb9a7dedd446b3a05accdd7f4943c1f5a2
molpopgen <krthornt@uci.edu>
2017-06-15 08:06:06 -0700

    more work on cpp sampler

--- doc/performance/sampler.cpp
+++ doc/performance/sampler.cpp
@@ -15,12 +15,32 @@ struct recorder_data
 struct recorder
 {
     std::vector<recorder_data> data;
+    recorder_data d;
     recorder(const std::uint32_t simlen) : data(std::vector<recorder_data>())
     {
         data.reserve(simlen);
     }
 
-    void operator()(const fwdpy11::singlepop_t * pop) const
+    void
+    operator()(const fwdpy11::singlepop_t& pop)
     {
+        double w = 0., s1 = 0., s2 = 0.;
+        for (auto&& dip : pop.diploids)
+            {
+                w += dip.w;
+                for (auto&& key : pop.gametes[dip.first].smutations)
+                    {
+                        s1 += pop.mutations[key].s;
+                    }
+                for (auto&& key : pop.gametes[dip.second].smutations)
+                    {
+                        s2 += pop.mutations[key].s;
+                    }
+            }
+        d.generation = pop.generation;
+        d.wbar = w;
+        d.s1 = s1 / static_cast<double>(pop.N);
+        d.s2 = s2 / static_cast<double>(pop.N);
+        data.push_back(d);
     }
 };
